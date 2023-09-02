PixelExperience/device_custom_sepolicy

mediatek: Add sepolicy for mediatek livedisplay
Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
Change-Id: I31f4e9ef942108bea1e6dd33b194183f88a73b37

mediatek/sepolicy.mk
@@ -1,0 +1,20 @@
+#
+# This policy configuration will be used by all mediatek products
+# that inherit from Lineage
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lineage/sepolicy/mediatek/dynamic \
+    device/lineage/sepolicy/mediatek/system
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/mediatek/dynamic \
+    device/lineage/sepolicy/mediatek/vendor
+endif

TheParasiteProject/device_parasite_sepolicy

mediatek/sepolicy.mk
@@ -1,0 +1,20 @@
+#
+# This policy configuration will be used by all mediatek products
+# that inherit from Lineage
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/parasite/sepolicy/mediatek/dynamic \
+    device/parasite/sepolicy/mediatek/system
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/parasite/sepolicy/mediatek/dynamic \
+    device/parasite/sepolicy/mediatek/vendor
+endif